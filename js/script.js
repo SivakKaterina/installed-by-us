6851614e92410e1627a2cb9d3a7d88ff99a3b488
SivakKaterina <72225534+SivakKaterina@users.noreply.github.com>
2022-08-10 13:49:00 +0300

    updated

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 // mobile-menu
 $(document).ready(function () {
   $('.burger').click(function (event) {
-    $('.burger, .headerMenu').toggleClass('active');
+    $('.burger, .headerMenu, ').toggleClass('active');
     $('body').toggleClass('lock');
   });
 });
+
+// $(document).ready(function () {
+//   $('.order').click(function (event) {
+//     $('.price').toggleClass('red');
+//   });
+// });
